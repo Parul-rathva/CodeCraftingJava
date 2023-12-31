72e685ff6800963129844568357f6eea69bc5ac3
Parul-rathva <p.rathva3041@gmail.com>
2023-12-31 19:05:05 +0530

    Diff way of printing

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Hello, Parul!\n");
+        System.out.println("Hello, Parul!");
         System.out.print("Hello, Parul!");
     }
 }
