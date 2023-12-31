462f7405f225b9ee03030c951e2b168dc3e80126
Parul-rathva <p.rathva3041@gmail.com>
2023-12-31 19:17:26 +0530

    Printing Star pattern using Print function

---
+++ StarPrinting.java
@@ -0,0 +1,19 @@
+// Q: Print Star pattern using Print function
+// Output: 
+//     *
+//     **
+//     *** 
+//     ****
+
+public class StarPrinting {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+        System.out.println("\nOR\n");
+
+        System.out.println("*\n**\n***\n****");
+    }
+}
